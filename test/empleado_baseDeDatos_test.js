fde9d1d0d92a45d22d9ab06c6b5812983757c1cf
Daniel <dany.orop@gmail.com>
2019-04-21 22:07:49 -0400

    mejora de nombres en empleado_baseDeDatos_test.js

--- test/empleado_baseDeDatos_test.js
+++ test/empleado_baseDeDatos_test.js
@@ -15,11 +15,11 @@ describe("Pruebas para la conexion de la base de datos y empleado", function () 
 
     it("Deberia crear correctamente un empleado con el nombre Mario Palacios", function (done) {   
         baseDeDatosTemporal.connect(urlBaseDeDatos, {}, function (error, baseDeDatos) {
-            var coleccion = baseDeDatos.collection('empleados');
+            var coleccionEmpleado = baseDeDatos.collection('empleados');
             var empleado = new Empleado(empleadoMario);
-            coleccion.insertMany(empleado, function (error, resultado) {
-                let empleadoResultado = resultado.ops[0];
-                expect(empleado.nombre).eq(empleadoResultado.nombre);
+            coleccionEmpleado.insertMany(empleado, function (error, resultado) {
+                let empleadoObtenido = resultado.ops[0];
+                expect(empleado.nombre).eq(empleadoObtenido.nombre);
                 done();
             });
             baseDeDatos.close();
@@ -28,11 +28,11 @@ describe("Pruebas para la conexion de la base de datos y empleado", function () 
 
     it("Deberia devolver el empleado Mario Palacios cuando se lo requiera", function (done) {
         baseDeDatosTemporal.connect(urlBaseDeDatos, {}, function (error, baseDeDatos) {
-            var coleccion = baseDeDatos.collection('empleados');
+            var coleccionEmpleado = baseDeDatos.collection('empleados');
             var empleado = new Empleado(empleadoMario);
-            coleccion.insertMany(empleado, function (err, resultadoInsert) {
-                let empleadoResultado = resultadoInsert.ops[0];
-                coleccion.find({ ci: empleadoResultado.ci }).toArray(function (err, resultadoFind) {
+            coleccionEmpleado.insertMany(empleado, function (err, resultadoInsert) {
+                let empleadoObtenido = resultadoInsert.ops[0];
+                coleccionEmpleado.find({ ci: empleadoObtenido.ci }).toArray(function (err, resultadoFind) {
                     let empleadoEncontrado = resultadoFind[0];
                     expect(empleado.nombre).eq(empleadoEncontrado.nombre);
                     done();
@@ -45,13 +45,13 @@ describe("Pruebas para la conexion de la base de datos y empleado", function () 
 
     it(`Deberia poder cambiar de tipo parcial a tipo comision dinamicamente para el empleado Mario`, function (done) {
             baseDeDatosTemporal.connect(urlBaseDeDatos, {}, function (error, baseDeDatos) {
-                var coleccion = baseDeDatos.collection('empleados');
+                var coleccionEmpleado = baseDeDatos.collection('empleados');
                 var empleado = new Empleado(empleadoMario);
-                coleccion.insertMany(empleado, function (err, resultadoInsert) {
-                    let empleadoResultado = resultadoInsert.ops[0];
-                    coleccion.updateOne({ ci: empleadoResultado.ci }, { $set: { tipo: "Comision" } },
+                coleccionEmpleado.insertMany(empleado, function (err, resultadoInsert) {
+                    let empleadoObtenido = resultadoInsert.ops[0];
+                    coleccionEmpleado.updateOne({ ci: empleadoObtenido.ci }, { $set: { tipo: "Comision" } },
                         function (err, resultadoUpdate) {
-                            coleccion.find({ ci: empleadoResultado.ci }).toArray(function (err, resultadoFind) {
+                            coleccionEmpleado.find({ ci: empleadoObtenido.ci }).toArray(function (err, resultadoFind) {
                                 let empleadoEncontrado = resultadoFind[0];
                                 expect(empleado.nombre).eq(empleadoEncontrado.nombre);
                                 expect(empleado.ci).eq(empleadoEncontrado.ci);
@@ -67,12 +67,12 @@ describe("Pruebas para la conexion de la base de datos y empleado", function () 
 
     // it(`Deberia poder eliminar al empleado Mario la base de datos si se desea`, function (done) {
     //         baseDeDatosTemporal.connect(urlBaseDeDatos, {}, function (error, baseDeDatos) {
-    //             var coleccion = baseDeDatos.collection('empleados');
+    //             var coleccionEmpleado = baseDeDatos.collection('empleados');
     //             var empleado = new Empleado(empleadoMario);
-    //             coleccion.insertMany(empleado, function (err, resultadoInsert) {
-    //                 let empleadoResultado = resultadoInsert.ops[0];
-    //                 coleccion.removeOne({ ci: empleadoResultado.ci }, function (err, resultadoRemove) {
-    //                     coleccion.find({ ci: empleadoResultado.ci }).toArray(function (err, resultadoFind) {
+    //             coleccionEmpleado.insertMany(empleado, function (err, resultadoInsert) {
+    //                 let empleadoObtenido = resultadoInsert.ops[0];
+    //                 coleccionEmpleado.removeOne({ ci: empleadoObtenido.ci }, function (err, resultadoRemove) {
+    //                     coleccionEmpleado.find({ ci: empleadoObtenido.ci }).toArray(function (err, resultadoFind) {
     //                         expect(resultadoFind.length).eq(0);
     //                         done();
     //                     });
