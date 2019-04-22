ee0c15f4223ccf1950fa02128a160c8565eb1ac8
Daniel <dany.orop@gmail.com>
2019-04-21 22:04:16 -0400

    Deberia poder cambiar de tipo parcial a tipo comision dinamicamente para el empleado Mario

--- test/empleado_baseDeDatos_test.js
+++ test/empleado_baseDeDatos_test.js
@@ -10,7 +10,7 @@ describe("Pruebas para la conexion de la base de datos y empleado", function () 
     var urlBaseDeDatos = 'mongodb://localhost/resthub';
     var empleadoMario = {
         nombre: "Mario Palacios", ci: "8339221", salario: "7000", fecha_nacimiento: "tue mar 28 2009", 
-        fecha_inicio: "tue mar 28 2009", tipo: "parcial", metodo_pago: "normal"
+        fecha_inicio: "tue mar 28 2009", tipo: "Parcial", metodo_pago: "normal"
     } 
 
     it("Deberia crear correctamente un empleado con el nombre Mario Palacios", function (done) {   
@@ -42,5 +42,44 @@ describe("Pruebas para la conexion de la base de datos y empleado", function () 
             baseDeDatos.close();
         });
     });
+
+    it(`Deberia poder cambiar de tipo parcial a tipo comision dinamicamente para el empleado Mario`, function (done) {
+            baseDeDatosTemporal.connect(urlBaseDeDatos, {}, function (error, baseDeDatos) {
+                var coleccion = baseDeDatos.collection('empleados');
+                var empleado = new Empleado(empleadoMario);
+                coleccion.insertMany(empleado, function (err, resultadoInsert) {
+                    let empleadoResultado = resultadoInsert.ops[0];
+                    coleccion.updateOne({ ci: empleadoResultado.ci }, { $set: { tipo: "Comision" } },
+                        function (err, resultadoUpdate) {
+                            coleccion.find({ ci: empleadoResultado.ci }).toArray(function (err, resultadoFind) {
+                                let empleadoEncontrado = resultadoFind[0];
+                                expect(empleado.nombre).eq(empleadoEncontrado.nombre);
+                                expect(empleado.ci).eq(empleadoEncontrado.ci);
+                                expect("Comision").eq(empleadoEncontrado.tipo);
+                                done();
+                            });
+                        });
+
+                });
+                baseDeDatos.close();
+            });
+        });
+
+    // it(`Deberia poder eliminar al empleado Mario la base de datos si se desea`, function (done) {
+    //         baseDeDatosTemporal.connect(urlBaseDeDatos, {}, function (error, baseDeDatos) {
+    //             var coleccion = baseDeDatos.collection('empleados');
+    //             var empleado = new Empleado(empleadoMario);
+    //             coleccion.insertMany(empleado, function (err, resultadoInsert) {
+    //                 let empleadoResultado = resultadoInsert.ops[0];
+    //                 coleccion.removeOne({ ci: empleadoResultado.ci }, function (err, resultadoRemove) {
+    //                     coleccion.find({ ci: empleadoResultado.ci }).toArray(function (err, resultadoFind) {
+    //                         expect(resultadoFind.length).eq(0);
+    //                         done();
+    //                     });
+    //                 });
+    //             });
+    //             baseDeDatos.close();
+    //         });
+    //     });
 
 });
