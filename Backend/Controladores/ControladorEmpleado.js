3f0e97dca10ed22e7e44919c28fb08ec383f029c
Daniel <dany.orop@gmail.com>
2019-05-27 23:27:38 -0400

    crear empleado enlace con backend

--- Backend/Controladores/ControladorEmpleado.js
+++ Backend/Controladores/ControladorEmpleado.js
@@ -1,6 +1,17 @@
 const express=require("express");
 const app=express();
 var  bodyParser = require("body-parser");
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended:true
@@ -18,7 +29,7 @@ app.get('/empleados',function(req,res){
     var interfazGeneradorBoletaDePago = new InterfazGeneradorBoletaDePago(new GeneradorBoletaDePago());
 });
 
-app.post('/empleado/new',function(req,res){
+app.post('/empleado/nuevo',function(req,res){
     console.log("post empleado");
 });
 
--- Frontend/src/components/pages/RegistroEmpleado.js
+++ Frontend/src/components/pages/RegistroEmpleado.js
@@ -1,14 +1,16 @@
 import React from 'react'; 
 import FormularioEmpleado from "../forms/FormularioEmpleado";
 import { Button } from "semantic-ui-react";
+import axios from "axios";
 
 class RegistroEmpleado extends React.Component {
     constructor(props) {
         super(props);
     }
     submit = (data) => {
-        console.log(data)
-        
+        axios.post('http://localhost:7000/empleado/nuevo', data)
+             .then(res => alert("Se creo un nuevo cliente"))
+             .catch(error => console.log(error))
     }
     volverAPaginaEmpleados() {
         this.props.history.goBack();
